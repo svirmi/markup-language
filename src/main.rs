f2ee0c50bdc47be7cf6765ec0b7205c69a58cb25
M.S <mixa.ru.ru@gmail.com>
2021-12-07 09:30:42 +0200

    Noraml and Comment cases cobered

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,21 @@ enum State {
     Lower,
 } // text transforms
 
-fn machine_cycle(state: State, c: char) -> (Option<char>, MachineState) {
+fn machine_cycle(state: State, c: char) -> (Option<char>, State) {
     use self::State::*;
     match state {
-        Normal => {}
-        Comment => {}
-        Upper => {}
-        Lower => {}
+        Normal => match c {
+            '#' => (None, Comment),
+            '^' => (None, Upper),
+            '_' => (None, Lower),
+            other => (Some(other), Normal),
+        },
+        Comment => match c {
+            '#' => (None, Normal),
+            other => (None, Other),
+        },
+        Upper => match c {},
+        Lower => match c {},
     }
 }
 
