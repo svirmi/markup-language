c69ee458a755cc834b5b9012be41f7382d7cffdc
M.S <mixa.ru.ru@gmail.com>
2021-12-07 09:12:30 +0200

    initial setup

---
+++ Readme.md
@@ -0,0 +1,1 @@
+##### Implementing Markup Language via state machine 
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+// text transforms
+enum State {
+    Normal,
+    Comment,
+    Upper,
+    Lower
+}
+
+fn machine_cycle(state: &MachineState, c: char) -> (Option<char>, MachineState) {
+    
+}
+
+fn main() {
+    println!("Hello, world!");
+}
